2b2b54677d9e6c4d5764b1ea8f7370bf0fc80145
JiYoungGoGo <chjy100418@naver.com>
2017-07-30 05:05:41 +0900

    포토 첫 화면

---
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PhotoActivity.java
@@ -0,0 +1,22 @@
+package com.gangnam4bungate.nuviseoul.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.gangnam4bungate.nuviseoul.R;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+
+    public void onClick1(View v){
+        Intent intent = new Intent(this, PhotoViewActivity.class);
+        startActivity(intent);
+    }
+}
