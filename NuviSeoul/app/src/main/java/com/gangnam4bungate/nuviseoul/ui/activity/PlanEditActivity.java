f40786b415a97a7e1b27773606d287c868cdd42d
seohyungju <shj890917@naver.com>
2017-08-06 02:24:22 +0900

    Merge branch 'seohyungju' of https://github.com/20170301/NuviSeoul
              # Conflicts:
              #	NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PlanEditActivity.java
    
              시작날짜 버튼 추가
    
              Fragment Planday -> Plandate  로 화면 전환

--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PlanEditActivity.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PlanEditActivity.java
@@ -4,7 +4,6 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.EditText;
 
 import com.gangnam4bungate.nuviseoul.R;
 import com.gangnam4bungate.nuviseoul.ui.common.CommonActivity;
@@ -12,27 +11,13 @@ import com.gangnam4bungate.nuviseoul.ui.common.CommonActivity;
 
 public class PlanEditActivity extends CommonActivity {
 
-    EditText travelSubjectText;
     PlanEditDayActivity planEditDayActivity;
     PlanEditDateActivity planEditDateActivity;
-
-//    int year, month, date;
-//    DatePickerDialog.OnDateSetListener mDateSetListener;
-//
-//    Button startDateButton;
-//    Button endDateButton;
-//    EditText startDateText;
-//    EditText endDateText;
-//    Button registerButton;
-
-
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_edit);
-//        setContentView(R.layout.activity_plan_edit_day);
 
 
         planEditDayActivity = new PlanEditDayActivity();
@@ -42,45 +27,6 @@ public class PlanEditActivity extends CommonActivity {
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.add(R.id.fragment_place, planEditDayActivity);
         fragmentTransaction.commit();
-//        View view = getLayoutInflater().inflate(R.layout.activity_plan_edit_day, null, false);
-//
-//
-//        startDateButton = (Button) view.findViewById(R.id.startDateButton);
-//        endDateButton = (Button) view.findViewById(R.id.endDateButton);
-//        startDateText = (EditText) view.findViewById(R.id.startDateText);
-//        endDateText = (EditText) view.findViewById(R.id.endDateText);
-//        registerButton = (Button) view.findViewById(R.id.registerButton);
-
-
-//        Calendar cal = Calendar.getInstance();
-//        year = cal.get(cal.YEAR);
-//        month = cal.get(cal.MONTH) + 1;
-//        date = cal.get(cal.DATE);
-//
-//        startDateButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startDateText.setText("aa");
-//                new DatePickerDialog(PlanEditActivity.this, mDateSetListener, year, month, date).show();
-//            }
-//        });
-//
-//        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-//            @Override
-//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-//                startDateText.setText(String.format("%d/%d/%d", year, month, dayOfMonth));
-//            }
-//        };
-
-//        registerButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FragmentManager fragmentManager = getFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_place, new PlanEditDateActivity());
-//                fragmentTransaction.commit();
-//            }
-//        });
 
 
     }
--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PlanEditDayActivity.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PlanEditDayActivity.java
@@ -1,28 +1,36 @@
 package com.gangnam4bungate.nuviseoul.ui.activity;
 
+import android.app.DatePickerDialog;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.gangnam4bungate.nuviseoul.R;
 
-public class PlanEditDayActivity extends Fragment {
+import java.util.Calendar;
+
+public class PlanEditDayActivity extends Fragment{
     Button startDateButton;
     Button endDateButton;
     EditText startDateText;
     EditText endDateText;
     Button registerButton;
+    int year, month, date;
+    DatePickerDialog.OnDateSetListener mDateSetListener;
 
         @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-            View view = inflater.inflate(R.layout.activity_plan_edit_day, container, false);
+            final View view = inflater.inflate(R.layout.activity_plan_edit_day, container, false);
 
             startDateButton = (Button) view.findViewById(R.id.startDateButton);
             endDateButton = (Button) view.findViewById(R.id.endDateButton);
@@ -30,15 +38,63 @@ public class PlanEditDayActivity extends Fragment {
             endDateText = (EditText) view.findViewById(R.id.endDateText);
             registerButton = (Button) view.findViewById(R.id.registerButton);
 
-            startDateButton.setOnClickListener(new View.OnClickListener() {
+        Calendar cal = Calendar.getInstance();
+        year = cal.get(cal.YEAR);
+        month = cal.get(cal.MONTH) + 1;
+        date = cal.get(cal.DATE);
+
+        startDateButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+//                startDateText.setText("aa");
+            DatePickerDialog dateDialog = new DatePickerDialog(getActivity(), startListener, year, month, date);
+            dateDialog.show();
+
+            }
+        });
+
+        endDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dateDialog = new DatePickerDialog(getActivity(), endListener, year, month, date);
+                dateDialog.show();
+            }
+        });
+
+
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                PlanEditDateActivity planEditDateActivity = new PlanEditDateActivity();
                 @Override
                 public void onClick(View v) {
-                    startDateText.setText("aa");
+                    FragmentManager fm = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_place, planEditDateActivity);
+                    fragmentTransaction.commit();
                 }
             });
 
         return view;
+
     }
+
+    private DatePickerDialog.OnDateSetListener startListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+
+            startDateText.setText(String.format("%d/%d/%d", year, month, dayOfMonth));
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener endListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+
+            endDateText.setText(String.format("%d/%d/%d", year, month, dayOfMonth));
+        }
+    };
+
 
 
 }
