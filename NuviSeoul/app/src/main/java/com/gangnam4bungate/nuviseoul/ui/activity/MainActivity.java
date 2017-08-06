a6d142ce5dd8390f935c8b11ebf1834defbe1652
nawonsuk <nawonsuk@gmail.com>
2017-08-06 12:05:14 +0900

    머지

--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/MainActivity.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/MainActivity.java
@@ -1,33 +1,110 @@
 package com.gangnam4bungate.nuviseoul.ui.activity;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.Button;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.android.volley.VolleyError;
 import com.gangnam4bungate.nuviseoul.R;
+import com.gangnam4bungate.nuviseoul.config.CODES;
+import com.gangnam4bungate.nuviseoul.holder.PlanAdapter;
+import com.gangnam4bungate.nuviseoul.model.AreaBaseModel;
+import com.gangnam4bungate.nuviseoul.network.NetworkManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.mystory.commonlibrary.network.MashupCallback;
 
-public class MainActivity extends FragmentActivity {
+import org.json.JSONObject;
 
-    //@BindView(R.id.cast_button_type_closed_caption) TextView textView;
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, MashupCallback {
+
+    private RecyclerView mRvPlan;
+    private PlanAdapter mPlanAdapter;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b = (Button)findViewById(R.id.button1);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(
-                        getApplicationContext(), // 현재 화면의 제어권자
-                        RecommendActivity.class); // 다음 넘어갈 클래스 지정
-                startActivity(intent); // 다음 화면으로 넘어간다
-            }
-        });
+        initLayout();
+    }
+
+    public void initLayout(){
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+
+        mPlanAdapter = new PlanAdapter();
+        mPlanAdapter.bindData(list);
+        mRvPlan = (RecyclerView) findViewById(R.id.rv_plan);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRvPlan.setLayoutManager(layoutManager);
+        mRvPlan.setAdapter(mPlanAdapter);
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        NetworkManager.getInstance().requestAreaBaseListInfo(this, CODES.API_CONTENTTYPE.FESTIVAL);
 
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onMashupSuccess(JSONObject object, String requestCode) {
+
+        if(requestCode != null && requestCode.equals(CODES.RequestCode.REQUEST_AREABASELIST)) {
+            try {
+                AreaBaseModel model = new Gson().fromJson(object.toString(), AreaBaseModel.class);
+                if(model != null){
+
+                }
+            } catch (Exception e) {
+                Log.e(CODES.TAG, "onMashupSucess Exception : " + e.getMessage());
+            }
+
+            Log.d(CODES.TAG, object.toString());
+        }
+    }
+
+    @Override
+    public void onMashupFail(VolleyError error, String requestCode) {
+        if(requestCode != null && requestCode.equals(CODES.RequestCode.REQUEST_AREABASELIST)) {
+
+            //Log.d(CODES.TAG, object.toString());
+        }
+    }
 }
