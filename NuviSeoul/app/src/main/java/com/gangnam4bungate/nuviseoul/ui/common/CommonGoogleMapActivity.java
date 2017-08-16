ec40dda44ab5dc6f1ccce1f436aea4ce67671448
korea-study <korea.study.dev@gmail.com>
2017-08-16 23:26:39 +0900

    DirectionFinder
    Direction Finder

--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/common/CommonGoogleMapActivity.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/common/CommonGoogleMapActivity.java
@@ -1,8 +1,10 @@
 package com.gangnam4bungate.nuviseoul.ui.common;
 
-import android.graphics.Color;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
@@ -12,7 +14,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by hschoi on 2017. 8. 06..
@@ -20,7 +23,8 @@ import com.google.android.gms.maps.model.PolylineOptions;
 
 public class CommonGoogleMapActivity extends FragmentActivity implements OnMapReadyCallback {
     protected GoogleMap mMap;
-    private  LatLng mLastedMarkLatLng;
+    private LatLng mLastedMarkLatLng;
+
     @Override
     /*protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +36,7 @@ public class CommonGoogleMapActivity extends FragmentActivity implements OnMapRe
         mapFragment.getMapAsync(this);
     }
    */
-    protected void onCreate(@Nullable  Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //onCreateView(savedInstanceState);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -41,6 +45,30 @@ public class CommonGoogleMapActivity extends FragmentActivity implements OnMapRe
         mapFragment.getMapAsync(this);*/
     }
 
+    private  void sendRequestWithDirection(String strOrigin,String strDestination)
+    {
+        if(strOrigin.isEmpty())
+        {
+            Toast.makeText(this,"Please, input origin address.",Toast.LENGTH_SHORT).show();
+            return ;
+        }
+
+        if(strDestination.isEmpty())
+        {
+            Toast.makeText(this,"Please, input destination address.",Toast.LENGTH_SHORT).show();
+            return ;
+        }
+
+        try
+        {
+            new DirectionFinder((DirectionFinderListener) this,strOrigin,strDestination).execute();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -57,8 +85,25 @@ public class CommonGoogleMapActivity extends FragmentActivity implements OnMapRe
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         LatLng seoul = new LatLng(37.52, 127.0);
-        mLastedMarkLatLng =seoul ;
-        mMap.addMarker(new MarkerOptions().position(seoul).title("Marker in Seoul"));
+        mLastedMarkLatLng = seoul;
+        mMap.addMarker(new MarkerOptions().position(seoul)
+                                           .title("Marker in Seoul")
+                                           /*.icon(BitmapDescriptorFactory.fromResource(com.google.android.gms.R.drawable.push_in))*/
+        );
+
+        //
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
         //1. 좌표 설정
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
 
@@ -114,14 +159,19 @@ public class CommonGoogleMapActivity extends FragmentActivity implements OnMapRe
                 polyline = googleMap.addPolyline(polyline_options);
                */
                 //2점 연결 - 시작점 , 마지막점
-                mMap.addPolyline(new PolylineOptions()
+               /* mMap.addPolyline(new PolylineOptions()
                         .color(Color.BLUE)
                         .width(5)
                         .geodesic(true)
                         .add(mLastedMarkLatLng)
                         .add(latLng)
                         //.add(new LatLng(arg0.latitude,arg0.longitude))
-                );
+                );*/
+
+                //String sOrigin=mLastedMarkLatLng.toString();
+                //String sDestination=latLng.toString();
+               // sendRequestWithDirection(mLastedMarkLatLng.toString(),latLng.toString());
+               sendRequestWithDirection("서울특별시 용산구 후암동 339","서울특별시 송파구 송파대로 441");
                 mLastedMarkLatLng = latLng;
             }
         });
---
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/common/Route.java
@@ -0,0 +1,20 @@
+package com.gangnam4bungate.nuviseoul.ui.common;
+
+/**
+ * Created by choi on 2017-08-16.
+ */
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public class Route {
+    public Distance distance;
+    public Duration duration;
+    public String endAddress;
+    public LatLng endLocation;
+    public String startAddress;
+    public LatLng startLocation;
+
+    public List<LatLng> points;
+}
