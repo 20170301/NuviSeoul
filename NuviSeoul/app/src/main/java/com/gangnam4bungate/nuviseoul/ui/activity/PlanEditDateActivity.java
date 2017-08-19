bb4e8c506ac2ff99c77dc75f44aeed7bfaa179ca
nawonsuk <nawonsuk@gmail.com>
2017-08-19 15:15:41 +0900

    머지

--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PlanEditDateActivity.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/PlanEditDateActivity.java
@@ -1,11 +1,13 @@
 package com.gangnam4bungate.nuviseoul.ui.activity;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gangnam4bungate.nuviseoul.R;
 
@@ -22,6 +24,16 @@ public class PlanEditDateActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.activity_plan_edit_date, container, false);
+        View v = inflater.inflate(R.layout.activity_plan_edit_date, container, false);
+
+        Button button = (Button) v.findViewById(R.id.button2);
+        if(button != null)
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    v.getContext().startActivity(new Intent(v.getContext(), RecommendActivity.class));
+                }
+            });
+        return v;
     }
 }
