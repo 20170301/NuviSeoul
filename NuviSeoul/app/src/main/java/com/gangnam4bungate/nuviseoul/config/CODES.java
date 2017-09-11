df67c230af85e90e8325597feb494aa66dbf0b5f
korea-study <korea.study.dev@gmail.com>
2017-09-11 13:02:07 +0900

    Merge branch 'master' of https://github.com/20170301/NuviSeoul

--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/config/CODES.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/config/CODES.java
@@ -7,10 +7,13 @@ package com.gangnam4bungate.nuviseoul.config;
 public class CODES {
     public static String TAG = "NuviSeoul";
     public static String DefaultDomain = "http://api.visitkorea.or.kr";
+    public static String NaverDomain = "https://openapi.naver.com";
     public static String Dev_ServiceKey = "Wasn0OGY43ReOgSy5nz8ZNURGTw4Y5MRPt%2B1rJw0xZCEbQG07s6n5hjqHHqzzIRBCP8U1H64Q0RIgAZPRWSPlA%3D%3D";
-
+    public static String NAVER_CLIENT_ID = "WA672XziAZbalmbhFN_9";
+    public static String NAVER_CLIENT_SECRET = "IVvaGuS7Tb";
     public static class URLCodes {
         public static String URL_AREABASELIST = "/openapi/service/rest/KorService/areaBasedList";
+        public static String URL_SEARCH = "/v1/search/blog?query=";
     }
 
     public static class CommonCodes {
@@ -24,6 +27,7 @@ public class CODES {
 
     public static class RequestCode {
         public static String REQUEST_AREABASELIST = "REQUEST_AREABASELIST";
+        public static String REQUEST_SEARCH = "REQUEST_SEARCH";
     }
 
     public static class API_CONTENTTYPE {
