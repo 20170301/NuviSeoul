af06430b7f2009bf02a778f16ab3cf7dfc44afe9
seohyungju <shj890917@naver.com>
2017-09-13 02:03:39 +0900

    Json Pasing <1>

--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/SearchActivity.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/activity/SearchActivity.java
@@ -4,28 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.VolleyError;
 import com.gangnam4bungate.nuviseoul.R;
 import com.gangnam4bungate.nuviseoul.config.CODES;
 import com.gangnam4bungate.nuviseoul.network.NetworkManager;
 import com.gangnam4bungate.nuviseoul.ui.common.CommonActivity;
+import com.google.gson.JsonParser;
 import com.mystory.commonlibrary.network.MashupCallback;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 /**
  * Created by wsseo on 2017. 7. 9..
  */
 
 public class SearchActivity extends CommonActivity implements MashupCallback {
-
+    ArrayList<String> blogList = new ArrayList<>();
+    ArrayList<String> linkList = new ArrayList<>();
+    TextView searchTextView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
+        searchTextView = (TextView)findViewById(R.id.searchTextView);
         Intent intent = getIntent();
         String value = intent.getStringExtra("editTextValue");
         NetworkManager.getInstance().requestNaverSearchInfo(this, value);
@@ -33,7 +42,32 @@ public class SearchActivity extends CommonActivity implements MashupCallback {
 
     @Override
     public void onMashupSuccess(JSONObject object, String requestCode) {
-        Log.d(CODES.TAG, "Search " + object.toString());
+        //Log.d(CODES.TAG, "Search " + object.toString());
+//        String jsonInfo = object.toString();
+//        JsonParser jsonParser = new JsonParser();
+
+        try
+        {
+            JSONObject jsonObject = new JSONObject(object.toString());
+            JSONArray item = jsonObject.getJSONArray("items");
+            String titleInfo = "";
+            String linkInfo = "";
+
+            for (int i = 0; i < item.length(); i++)
+            {
+                titleInfo = item.getJSONObject(i).getString("title");
+                linkInfo = item.getJSONObject(i).getString("link");
+
+                searchTextView.append(titleInfo + "\n");
+                searchTextView.append(linkInfo + "\n");
+            }
+//            searchTextView.setText(""+titleInfo+"\n"+""+linkInfo);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
 
     }
 
