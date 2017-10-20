bbd5c6b415c77bdb24f84b0a5b5ff10fcfb585d6
korea-study <korea.study.dev@gmail.com>
2017-10-21 03:44:07 +0900

    change commit
    change commit

--- NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/common/CommonGoogleMapActivity.java
+++ NuviSeoul/app/src/main/java/com/gangnam4bungate/nuviseoul/ui/common/CommonGoogleMapActivity.java
@@ -240,6 +240,11 @@ public class CommonGoogleMapActivity extends AppCompatActivity implements OnMapR
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.house_flag))
                 .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
                 .position(new LatLng(41.889, -87.622)));*/
+
+/*-------------------------------------------------------------------------------------------------*/
+        // Setting an info window adapter allows us to change the both the contents and look of the
+        // info window.
+        //mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
 /*-------------------------------------------------------------------------------------------------*/
         //기존에 있는 정보 가져오기
         if(this.mType==0){
